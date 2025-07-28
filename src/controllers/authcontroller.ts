c0f074b55e588a241d546f05f37489aeb7aa6e47
AryanDadhich <rajkumardadheech6@gmail.com>
2025-07-28 10:54:52 +0530

    auth

--- src/controllers/authcontroller.ts
+++ src/controllers/authcontroller.ts
@@ -1,5 +1,5 @@
 import {Request, Response} from "express";
-import User from "../models/user"
+import User from "../models/user" //user model to interect with mongodb
 import {generateAccessToken, generateRefreshToken} from '../utils/generateToken.js';
 
 export const registerUser = async(req:Request,res:Response): Promise<void> => {
@@ -9,10 +9,49 @@ export const registerUser = async(req:Request,res:Response): Promise<void> => {
         //validation
         if(!name||!email||!password){
             res.status(400).json({message:"All fields are required"});
+            return;
         }
+        
+        //check if user is alredy exist
+        const UserExists = await User.findOne ({email});
+        if(UserExists){
+            res.status(400).json({message : " User alredy exists"});
+            return;
+        }
+
+        // Create New User 
+        const user = await User.create({
+            name,email,password,
+        });
+
+        //Generate Token 
+        const accessToken = generateAccessToken(User._id.toString());
+        const RefreshToken = generateRefreshToken(User._id.toString());
+
+        //Set refresh token in HTTP-only Cookie
+        res.cookie("refreshtoken",RefreshToken,{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite : "strict",
+            maxAge : 7*24*60*60*1000 //7 days
+        });
+
+        //Return user data & token
+        res.status(201).json({
+            message:"User registerd successfully",
+            user:{
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            },
+            accessToken,
+            RefreshToken,
+        })
     }
-catch(err){
-    console.log("error",err)
+catch(error : any ){
+    console.log("Register error",error);
+    res.status(500).json({message: "Internal server error"})
 }
     //validate input
 }
