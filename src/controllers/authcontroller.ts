00a91dc0315e893dc45d3fe9589d3990b5cbc5ea
AryanDadhich <rajkumardadheech6@gmail.com>
2025-07-28 11:10:00 +0530

    registerauth

--- src/controllers/authcontroller.ts
+++ src/controllers/authcontroller.ts
@@ -32,7 +32,7 @@ export const registerUser = async(req:Request,res:Response): Promise<void> => {
         res.cookie("refreshtoken",RefreshToken,{
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite : "strict",
+            sameSite : "strict", //protect against cross site requests
             maxAge : 7*24*60*60*1000 //7 days
         });
 
