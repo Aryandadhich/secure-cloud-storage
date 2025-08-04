a9ca4c33970967b5b3779903d494609378c04345
AryanDadhich <rajkumardadheech6@gmail.com>
2025-08-04 12:43:08 +0530

    authRoutes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,2 +1,10 @@
 import express from "express";
-import 
+import {registerUser , loginUser} from '../controllers/authcontroller';
+
+const router = express.Router();
+
+//routes
+router.post('/register',registerUser)
+router.post('/login',loginUser)
+
+export default router;
