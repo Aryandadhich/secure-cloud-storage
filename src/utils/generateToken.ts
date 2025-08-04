d0357ca8b200fb136b5131815b66da86340e7bed
AryanDadhich <rajkumardadheech6@gmail.com>
2025-08-04 12:25:21 +0530

    Auth

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -5,7 +5,7 @@ export const generateAccessToken = (userId : string) : string => {
     return jwt.sign({id: userId}, process.env.JWT_SECRET as string, {expiresIn:'15m'});
 }
 
-//Refreshj Token
+//Refresh Token
 export const generateRefreshToken = (userId: string): string => {
     return jwt.sign({id: userId}, process.env.JWT_REFRESH_SECRET as string, {expiresIn: '7d'});
 }
